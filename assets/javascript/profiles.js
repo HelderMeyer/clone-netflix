1efc4d8acacf0d9b276c48ceb51ccb8833513e25
Helder Meyer <helder.meyer3010@gmail.com>
2022-08-29 17:19:35 -0300

    First commit

---
+++ assets/javascript/profiles.js
@@ -0,0 +1,57 @@
+const effect1 = document.getElementById("h3_1");
+const effect2 = document.getElementById("h3_2");
+const effect3 = document.getElementById("h3_3");
+const effect4 = document.getElementById("h3_4");
+
+const img1 = document.getElementById("img_1");
+const img2 = document.getElementById("img_2");
+const img3 = document.getElementById("img_3");
+const img4 = document.getElementById("img_4");
+
+effect1.onmouseover = function (){
+
+    img1.style.border = "5px solid white";
+
+}
+
+effect1.onmouseout = function (){
+
+    img1.style.border = "";
+
+}
+
+effect2.onmouseover = function (){
+
+    img2.style.border = "5px solid white";
+
+}
+
+effect2.onmouseout = function (){
+
+    img2.style.border = "";
+
+}
+
+effect3.onmouseover = function (){
+
+    img3.style.border = "5px solid white";
+
+}
+
+effect3.onmouseout = function (){
+
+    img3.style.border = "";
+
+}
+
+effect4.onmouseover = function (){
+
+    img4.style.border = "5px solid white";
+
+}
+
+effect4.onmouseout = function (){
+
+    img4.style.border = "";
+
+}
